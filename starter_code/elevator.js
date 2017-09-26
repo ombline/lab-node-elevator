008fa4d3fe669707980bc69fc419873e98c6cf34
ombline <ombline90@hotmail.com>
2017-09-26 09:12:02 +0200

    Not finished

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,38 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.direction = "up";
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.interval = null;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.interval = setInterval(() => update(), 1000);
+  }
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  update() {
+    this.log();
+  }
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {
+    if (this.floor <= this.MAXFLOOR) {
+      this.floor++;
+    }
+  }
+  floorDown() {
+    if (this.floor >= 0) {
+      this.floor--;
+    }
+  }
+  call() {}
+  log() {
+    console.log(`Direction: ${this.direction}, | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,7 @@
-const Elevator = require('./elevator.js');
+const Elevator = require("./elevator.js");
+const Person = require("./person.js");
+
+const elevator = new Elevator();
+elevator.log();
+elevator.floorUp();
+elevator.log();
